9fffcfad2829d74bc9a73ff33fe5c56fe803e405
herman-xphp <hermanxphp@gmail.com>
2025-10-12 20:48:32 +0800

    Request

--- handler_test.go
+++ handler_test.go
@@ -48,3 +48,20 @@ func TestServerMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.Method)
+		fmt.Fprintln(w, r.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
