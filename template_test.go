4289fe8d10455ec60bf04d76d7acce1216b693d1
herman-xphp <hermanxphp@gmail.com>
2025-10-18 23:04:29 +0800

    Template Caching

--- template_test.go
+++ template_test.go
@@ -1,7 +1,6 @@
 package belajar_golang_web
 
 import (
-	"embed"
 	"fmt"
 	"html/template"
 	"io"
@@ -61,9 +60,6 @@ func TestTemplateDirectory(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
-
-//go:embed templates/*.gohtml
-var templates embed.FS
 
 func TemplateEmbed(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFS(templates, "templates/*.gohtml"))
